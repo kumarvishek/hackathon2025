d4a3a485921161083ac3bacc62b8fa050b59b617
Salman <slmnshq25@gmail.com>
2025-06-14 16:50:22 +0530

    Update - Timesheet API

--- src/main/java/com/squadseven/timesheet/util/Constants.java
+++ src/main/java/com/squadseven/timesheet/util/Constants.java
@@ -4,4 +4,5 @@ public class Constants {
     public static final int INTERNAL_SERVER_ERROR_ID= 500;
     public static final int ACTIVITY_ERROR_ID= 8000;
     public static final String INTERNAL_SERVER_ERROR_MESSAGE= "Looks like something went wrong";
+    public static final String DRAFT = "Draft";
 }
