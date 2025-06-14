34de67a76efb6f398824c2fd0e98e4c08d1e0dfd
Salman <slmnshq25@gmail.com>
2025-06-14 15:21:29 +0530

    Added - Global Exception Handler

---
+++ src/main/java/com/squadseven/timesheet/exception/GlobalExceptionHandler.java
@@ -0,0 +1,22 @@
+package com.squadseven.timesheet.exception;
+
+
+import com.squadseven.timesheet.model.BaseError;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import static com.squadseven.timesheet.util.Constants.INTERNAL_SERVER_ERROR_ID;
+import static com.squadseven.timesheet.util.Constants.INTERNAL_SERVER_ERROR_MESSAGE;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<BaseError> handleGenericException(Exception ex) {
+        BaseError error = new BaseError(INTERNAL_SERVER_ERROR_ID,INTERNAL_SERVER_ERROR_MESSAGE);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(error);
+    }
+}
