6e883aa8748a6dcff6b9f45ac53855cfc20d59b3
ritika <ritika.k@hanatechpvt.com>
2025-06-14 16:47:45 +0530

    Insert project

---
+++ src/main/java/com/squadseven/timesheet/controller/ProjectController.java
@@ -0,0 +1,29 @@
+package com.squadseven.timesheet.controller;
+
+import com.squadseven.timesheet.helper.JsonResponse;
+import com.squadseven.timesheet.model.Project;
+import com.squadseven.timesheet.service.ProjectService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/projects")
+@RequiredArgsConstructor
+public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    // Create or update a project
+    @PostMapping
+    public ResponseEntity<Project> createProject(@RequestBody Project project) {
+        Project savedProject = projectService.saveProject(project);
+
+        return ResponseEntity.ok(savedProject);
+    }
+}
